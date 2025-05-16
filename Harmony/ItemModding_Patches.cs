96e6faa697e421dff8ac82bd1579a0959cba607c
UnderDoug <douglas.marken@gmail.com>
2025-05-16 14:45:40 +1000

    tidied up unused code.

--- Harmony/ItemModding_Patches.cs
+++ Harmony/ItemModding_Patches.cs
@@ -17,40 +17,6 @@ namespace UD_Ductape_Mod.Harmony
     [HarmonyPatch]
     public static class ItemModding_Patches
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(
-            declaringType: typeof(ItemModding),
-            methodName: nameof(ItemModding.ModificationApplicable),
-            argumentTypes: new Type[] { typeof(string), typeof(GameObject), typeof(GameObject), typeof(string) },
-            argumentVariations: new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal }
-            )]
-        public static bool ModificationApplicable_AllowFourth_Prefix(ref bool __result, string Name, GameObject Object, string Key, GameObject Actor = null)
-        {
-            if(false && Name == nameof(Mod_UD_Ductape) && Key != null)
-            {
-                Debug.Entry(4,
-                    $"{nameof(ItemModding)}." + 
-                    $"{nameof(ItemModding.ModificationApplicable)}(" +
-                    $"Name: {Name ?? NULL}, " +
-                    $"Key: {Key ?? NULL})",
-                    Indent: Debug.LastIndent + 1, Toggle: true
-                    );
-            }
-            if (Name == nameof(Mod_UD_Ductape) && Key == null)
-            {
-                Debug.Entry(4,
-                    $"{nameof(ItemModding)}." +
-                    $"{nameof(ItemModding.ModificationApplicable)}(" +
-                    $"Name: {Name ?? NULL}, " +
-                    $"Key: {Key ?? NULL})",
-                    Indent: 0, Toggle: true
-                    );
-                // __result = ItemModding.ModificationApplicable(Name, Object, Actor, nameof(Mod_UD_Ductape));
-                // return false;
-            }
-            return true;
-        }
-
         [HarmonyPostfix]
         [HarmonyPatch(
             declaringType: typeof(ItemModding),
